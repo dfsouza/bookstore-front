8240e2c45c244df2f5545b5abb5a3d36e880472c
dfsouza <danielfsouza@gmail.com>
2023-05-02 22:44:42 -0300

    UPDATE para livro implementado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
+import { LivroUpdateComponent } from './components/views/livro/livro-update/livro-update.component';
 import { LivroCreateComponent } from './components/views/livro/livro-create/livro-create.component';
 import { CategoriaUpdateComponent } from './components/views/categoria/categoria-update/categoria-update.component';
 import { CategoriaDeleteComponent } from './components/views/categoria/categoria-delete/categoria-delete.component';
 import { CategoriaCreateComponent } from './components/views/categoria/categoria-create/categoria-create.component';
 import { CategoriaReadComponent } from './components/views/categoria/categoria-read/categoria-read.component';
 import { HomeComponent } from './components/views/home/home.component';
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LivroReadAllComponent } from './components/views/livro/livro-read-all/livro-read-all.component';
 
@@ -36,6 +37,10 @@ const routes: Routes = [
   {
     path: 'categorias/:id_cat/livros/create',
     component: LivroCreateComponent
+  },
+  {
+    path: 'categorias/:id_cat/livros/:id/update',
+    component: LivroUpdateComponent
   }
 ];
 
--- src/app/components/views/livro/livro.service.ts
+++ src/app/components/views/livro/livro.service.ts
@@ -1,3 +1,4 @@
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { livro } from './livro.model';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
@@ -11,11 +12,33 @@ export class LivroService {
 
   baseUrl =  environment.baseUrl;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private _snack: MatSnackBar) { }
 
   findAllByCategoria(id_cat: String): Observable<livro[]>{
      const url = `${this.baseUrl}/livros?categoria=${id_cat}`
      return this.http.get<livro[]>(url)   
   }
 
+  findById(id: String):Observable<livro>{
+    const url = `${this.baseUrl}/livros?categoria=${id}`
+    return this.http.get<livro>(url)
+  }
+
+    update(livro: livro):Observable<livro>{
+    const url = `${this.baseUrl}/livros/${livro.id}`
+    return this.http.put<livro>(url, livro)
+  }
+
+  create(livro: livro, id_cat: String): Observable<livro> {
+    const url = `${this.baseUrl}/livros?categoria=${id_cat}`
+    return this.http.post<livro>(url, livro)
+  }
+
+  mensagem(str: String): void{
+    this._snack.open(`${str}`, 'OK', {
+      horizontalPosition: 'center',
+      verticalPosition: 'bottom',
+      duration: 3000
+    })
+  }
 }
